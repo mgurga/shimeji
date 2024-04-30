0d3bf4e109f6fab4cbc166f5f00ee5a8f25a998e
mgurga <motiurga@gmail.com>
2024-04-29 18:57:54 -0700

    move window functions to Utils class

--- src/Shimeji.cpp
+++ src/Shimeji.cpp
@@ -1,81 +1,30 @@
 #include "Shimeji.hpp"
 
-bool Shimeji::setShape(Window wnd, const sf::Image& image) {
-    Display* display = XOpenDisplay(NULL);
-
-    int event_base;
-    int error_base;
-    if (!XShapeQueryExtension(display, &event_base, &error_base)) {
-        XCloseDisplay(display);
-        return false;
-    }
-
-    const sf::Uint8* pixelData = image.getPixelsPtr();
-
-    Pixmap pixmap = XCreatePixmap(display, wnd, image.getSize().x, image.getSize().y, 1);
-    GC gc = XCreateGC(display, pixmap, 0, NULL);
-
-    XSetForeground(display, gc, 1);
-    XFillRectangle(display, pixmap, gc, 0, 0, image.getSize().x, image.getSize().y);
-
-    XSetForeground(display, gc, 0);
-    bool transparentPixelFound = false;
-    unsigned int rectLeft = 0;
-    for (unsigned int y = 0; y < image.getSize().y; y++) {
-        for (unsigned int x = 0; x < image.getSize().x; x++) {
-            const bool isTransparentPixel = (pixelData[y * image.getSize().x * 4 + x * 4 + 3] == 0);
-            if (isTransparentPixel && !transparentPixelFound) {
-                transparentPixelFound = true;
-                rectLeft = x;
-            } else if (!isTransparentPixel && transparentPixelFound) {
-                XFillRectangle(display, pixmap, gc, rectLeft, y, x - rectLeft, 1);
-                transparentPixelFound = false;
-            }
+void Shimeji::update() {
+    if (window->isOpen()) {
+        sf::Event event;
+        while (window->pollEvent(event)) {
+            if (event.type == sf::Event::Closed ||
+                event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+                window->close();
         }
 
-        if (transparentPixelFound) {
-            XFillRectangle(display, pixmap, gc, rectLeft, y, image.getSize().x - rectLeft, 1);
-            transparentPixelFound = false;
-        }
-    }
-
-    XShapeCombineMask(display, wnd, ShapeBounding, 0, 0, pixmap, ShapeSet);
-
-    XFreeGC(display, gc);
-    XFreePixmap(display, pixmap);
-    XFlush(display);
-    XCloseDisplay(display);
-    return true;
-}
-
-bool Shimeji::setTransparency(Window wnd, unsigned char alpha) {
-    Display* display = XOpenDisplay(NULL);
-    unsigned long opacity = (0xffffffff / 0xff) * alpha;
-    Atom property = XInternAtom(display, "_NET_WM_WINDOW_OPACITY", false);
-    if (property != None) {
-        XChangeProperty(display, wnd, property, XA_CARDINAL, 32, PropModeReplace,
-                        (unsigned char*)&opacity, 1);
-        XFlush(display);
-        XCloseDisplay(display);
-        return true;
-    } else {
-        XCloseDisplay(display);
-        return false;
+        window->clear(sf::Color::Transparent);
+        window->draw(spr);
+        window->display();
     }
 }
 
-void Shimeji::resizeImage(const sf::Image& originalImage, sf::Image& resizedImage) {
-    const sf::Vector2u originalImageSize{originalImage.getSize()};
-    const sf::Vector2u resizedImageSize{resizedImage.getSize()};
-    for (unsigned int y{0u}; y < resizedImageSize.y; ++y) {
-        for (unsigned int x{0u}; x < resizedImageSize.x; ++x) {
-            unsigned int origX{static_cast<unsigned int>(static_cast<double>(x) /
-                                                         resizedImageSize.x * originalImageSize.x)};
-            unsigned int origY{static_cast<unsigned int>(static_cast<double>(y) /
-                                                         resizedImageSize.y * originalImageSize.y)};
-            resizedImage.setPixel(x, y, originalImage.getPixel(origX, origY));
-        }
-    }
-}
+void Shimeji::update_frame(int num) {
+    auto base_img = get_texture().copyToImage();
+    sf::Image scaled_img;
+    scaled_img.create(base_img.getSize().x * scale, base_img.getSize().y * scale);
+    Utils::resizeImage(base_img, scaled_img);
 
-#undef None
+    Utils::setShape(window->getSystemHandle(), scaled_img);
+    Utils::setTransparency(window->getSystemHandle(), 255);
+
+    spr.setTexture(get_texture(), true);
+    spr.setPosition(0, 0);
+    spr.setScale({scale, scale});
+}
--- src/Shimeji.hpp
+++ src/Shimeji.hpp
@@ -1,14 +1,9 @@
 #pragma once
 
-// clang-format off
-#include <SFML/Graphics.hpp>
-
-#include <X11/Xatom.h>
-#include <X11/extensions/shape.h>
-// clang-format on
 #include <iostream>
 
 #include "ShimejiPack.hpp"
+#include "Utils.hpp"
 
 class Shimeji {
 public:
@@ -20,36 +15,8 @@ public:
         update_frame(0);
     }
 
-    void update() {
-        sf::Vector2i grabbedOffset;
-        bool grabbedWindow = false;
-        if (window->isOpen()) {
-            sf::Event event;
-            while (window->pollEvent(event)) {
-                if (event.type == sf::Event::Closed ||
-                    event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
-                    window->close();
-            }
-
-            window->clear(sf::Color::Transparent);
-            window->draw(spr);
-            window->display();
-        }
-    }
-
-    void update_frame(int num) {
-        auto base_img = get_texture().copyToImage();
-        sf::Image scaled_img;
-        scaled_img.create(base_img.getSize().x * scale, base_img.getSize().y * scale);
-        resizeImage(base_img, scaled_img);
-
-        setShape(window->getSystemHandle(), scaled_img);
-        setTransparency(window->getSystemHandle(), 255);
-
-        spr.setTexture(get_texture(), true);
-        spr.setPosition(0, 0);
-        spr.setScale({scale, scale});
-    }
+    void update();
+    void update_frame(int num);
 
     sf::Texture& get_texture() { return image_pack.get_texture(curframe); }
     int get_x() { return x; }
@@ -62,8 +29,4 @@ private:
     ShimejiPack& image_pack;
     sf::RenderWindow* window;
     sf::Sprite spr;
-
-    bool setShape(Window wnd, const sf::Image& image);
-    bool setTransparency(Window wnd, unsigned char alpha);
-    void resizeImage(const sf::Image& originalImage, sf::Image& resizedImage);
 };
