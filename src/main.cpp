c636add2c8fda4253ede38c2c8ac681d43698422
mgurga <motiurga@gmail.com>
2024-05-08 17:26:45 -0700

    limit update loop to 60 fps

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 
@@ -9,5 +10,14 @@ int main() {
     Manager m(scale);
     m.create_shimeji(3);
 
-    while (true) { m.update(); }
+    while (true) {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        m.update();
+
+        auto end = std::chrono::high_resolution_clock::now();
+        while (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() < 17) {
+            end = std::chrono::high_resolution_clock::now();
+        }
+    }
 }
